248661fc2076be482da6ba2825e6d4b03874b961
김지훈 <87347699+jomulagy@users.noreply.github.com>
2026-01-04 20:57:58 +0900

    .init

---
+++ src/main/java/com/example/dataConsumer/DataCounsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCounsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCounsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dataCounsumerApplication/DataCounsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataCounsumerApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCounsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
